445ebd1ab5e650934e73b3ba1ddea79941521d3c
Bibichkov Igor <31080547+Bibi4kovIgor@users.noreply.github.com>
2023-08-08 21:30:55 +0300

    Update Main.java
    Added demo methods tests

--- src/main/java/edu/lemonschool/Main.java
+++ src/main/java/edu/lemonschool/Main.java
@@ -2,6 +2,14 @@ package edu.lemonschool;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> integerList = new LinkedList<>();
+        System.out.println(integerList.getSize());
+
+        integerList.add(12);
+        integerList.add(13);
+        integerList.add(7);
+
+        System.out.println(integerList);
+        System.out.println(integerList.get(1));
     }
 }
