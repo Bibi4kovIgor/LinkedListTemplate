7d628b5c8535df57035d6bced8247c3477b09a9b
Bibichkov Igor <31080547+Bibi4kovIgor@users.noreply.github.com>
2023-08-08 21:30:02 +0300

    Update and rename LinkedList.java to Linked.java

---
+++ src/main/java/edu/lemonschool/list/Linked.java
@@ -0,0 +1,10 @@
+package edu.lemonschool.list;
+
+public interface List<E> {
+    void add(E element);
+    void add(Integer index, E element);
+    void remove(Integer index);
+    E update(Integer index);
+    E get(Integer index);
+    E[] toArray();
+}
