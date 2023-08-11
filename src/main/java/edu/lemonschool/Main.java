19e2b1132ebebc5771b3c6a9eeedd57e266bd169
Bibichkov Igor <31080547+Bibi4kovIgor@users.noreply.github.com>
2023-08-11 23:59:37 +0300

    Update Main.java
    Added 
    - test cases for exception

--- src/main/java/edu/lemonschool/Main.java
+++ src/main/java/edu/lemonschool/Main.java
@@ -1,7 +1,10 @@
 package edu.lemonschool;
 
 public class Main {
+    private final static Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+
+
         List<Integer> integerList = new LinkedList<>();
         System.out.println(integerList.getSize());
 
@@ -11,5 +14,29 @@ public class Main {
 
         System.out.println(integerList);
         System.out.println(integerList.get(1));
+
+        // test case to show try...finally form of critical section exception
+        try {
+            integerList.add(2, 777);
+            System.out.println(integerList);
+        } /*catch (IllegalArgumentException exception) {
+            LOG.error(exception.getMessage());
+        }*/ finally {
+            System.out.println("Finally block for 777");
+        }
+
+        // It will be raised an exception, with writing into the log file (logs/io.log)
+        try {
+            integerList.add(7, 111);
+            System.out.println(integerList);
+        } catch (IllegalArgumentException exception) {
+            LOG.error(exception.getMessage());
+        } finally {
+            System.out.println("Finally block for 111");
+        }
+
+        integerList.add(4, 222);
+        System.out.println(integerList);
     }
+
 }
