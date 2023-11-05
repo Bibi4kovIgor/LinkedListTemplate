0282f724d3b04efa30abae198d67a7903247a0c9
Bibichkov Igor <31080547+Bibi4kovIgor@users.noreply.github.com>
2023-11-05 20:33:38 +0200

    Update LinkedList.java
    Reset to templaate

--- src/main/java/edu/lemonschool/list/LinkedList.java
+++ src/main/java/edu/lemonschool/list/LinkedList.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class LinkedList<E> implements List<E> {
     //(Composition)
     private final static class Node<E> {
-        private E value;
-        private Node<E> next;
-        private Node<E> previous;
+        E value;
+        Node<E> next;
+        Node<E> previous;
 
         public Node(E value, Node<E> next, Node<E> previous) {
             this.value = value;
@@ -49,79 +49,14 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public void add(E value) {
-        addElementToLastPosition(value);
+
     }
 
     @Override
     public void add(Integer position, E value) throws IllegalArgumentException {
-        if(position < 0 || position > size) {
-            throw new IllegalArgumentException("Position must be greater than 0 nad less than size + 1");
-        }
-        Node<E> newNode = new Node<>(value);
-        if(position == 0) {
-            addElementToFirstPosition(value);
-        } else if(position == size) {
-            addElementToLastPosition(value);
-        } else {
-            addElementToMiddlePosition(position, newNode);
-        }
+
     }
 
-    private void addElementToLastPosition(E value) {
-        Node<E> newNode = new Node<>(value);
-        if (this.size == 0) {
-            this.head = this.tail = newNode;
-        } else {
-            tail.next = newNode;
-            newNode.previous = tail;
-            tail = newNode;
-        }
-        this.size++;
-    }
-
-    private void addElementToMiddlePosition(int position, Node<E> newNode) {
-        Node<E> temp;
-        if (position > size / 2) {
-            temp = getNodeFromEnd(position);
-        } else {
-            temp = getNodeFromBegin(position);
-        }
-        Node<E> next = temp.next;
-        newNode.next = next;
-        newNode.previous = temp;
-        next.previous = newNode;
-        temp.next = newNode;
-        size++;
-    }
-
-    private Node<E> getNodeFromBegin(int position) {
-        Node<E> temp;
-        temp = head;
-        for (int i = 0; i <= position; i++) {
-            temp = temp.next;
-        }
-        return temp;
-    }
-
-    private Node<E> getNodeFromEnd(int position) {
-        Node<E> temp;
-        temp = tail;
-        for (int i = size - 1; i >= position; i--) {
-            temp = temp.previous; // previous
-        }
-        return temp;
-    }
-
-    private void addElementToFirstPosition(E value) {
-        Node<E> newNode = new Node<>(value);
-        if (size == 0) {
-            head = tail = newNode;
-        } else {
-            newNode.next = head;
-            head = newNode;
-        }
-        size++;
-    }
 
     @Override
     public void remove(Integer index) {
@@ -135,23 +70,13 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public E get(Integer index) {
-        Node<E> temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp.next;
-        }
-        return temp.value;
+       
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public E[] toArray() {
-        Object[] result = new Object[size];
-        Node<E> temp = head;
-        for (int i = 0; i < size; i++) {
-            result[i] = temp.value;
-            temp = temp.next;
-        }
-        return (E[])result;
+      
     }
 
     @Override
@@ -161,6 +86,6 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public String toString() {
-        return Arrays.toString(toArray());
+      
     }
 }
